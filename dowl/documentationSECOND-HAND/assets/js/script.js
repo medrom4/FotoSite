bd553d77876ee98b1376ec1cbe664291b2c37271
medrom4 <medikroma@gmail.com>
2020-01-04 22:23:25 +0300

    site-second-

---
+++ dowl/documentationSECOND-HAND/assets/js/script.js
@@ -0,0 +1,15 @@
+/*!
+ * Documenter 2.0
+ * http://rxa.li/documenter
+ *
+ * Copyright 2011, Xaver Birsak
+ * http://revaxarts.com
+ *
+ */
+ 
+jQuery(document).ready(function($){
+	jQuery('.sidebar-action').bind('click',function(){
+		$('#documenter_sidebar').toggleClass('active');
+		
+	});
+});
